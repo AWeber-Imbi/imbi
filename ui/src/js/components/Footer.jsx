6eede187b6440650fbc70e1d132b67a5cd08b1bc
Gavin M. Roy <gavinr@aweber.com>
2021-01-01 19:55:12 -0500

    Add the AWeber logo as a component instead of font

--- ui/src/js/components/Footer.jsx
+++ ui/src/js/components/Footer.jsx
@@ -2,11 +2,20 @@ import PropTypes from "prop-types";
 import React from "react"
 import {useTranslation} from "react-i18next"
 
+import {default as Logo} from "./AWeber"
+
 function Footer({service, version}) {
   const {t} = useTranslation()
   return (
-    <footer className="flex-shrink h-10 bg-white border-t border-gray-400 align-middle text-sm p-2 text-gray-500">
-      {service} v{version} &mdash; <a href="/api-docs/">{t("footer.apiDocumentation")}</a>
+    <footer className="flex-shrink flex h-10 bg-white border-t border-gray-400 align-middle text-sm text-gray-500">
+      <div className="flex-auto p-2">
+        {service} v{version} &mdash; <a href="/api-docs/">{t("footer.apiDocumentation")}</a>
+      </div>
+      <div className="flex-shrink h-8 w-8 p-1 mr-2">
+        <a href="https://aweber.com">
+          <Logo className="text-blue-700 h-8 w-8" />
+        </a>
+      </div>
     </footer>
   )
 }
