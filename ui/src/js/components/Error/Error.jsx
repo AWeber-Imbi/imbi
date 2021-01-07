df0e7562060f27428f8061a8e92e314a1515fc52
Gavin M. Roy <gavinr@aweber.com>
2021-01-06 20:51:37 -0500

    Extract Error to a component

---
+++ ui/src/js/components/Error/Error.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import React from "react"
+import {useTranslation} from "react-i18next";
+
+import {Icon} from ".."
+
+function Error({children}) {
+  const {t} = useTranslation()
+  return (
+    <div className="container mx-auto my-auto max-w-xs bg-red-50 shadow rounded-lg p-5 text-red-700">
+      <Icon icon="fas exclamation-circle" />
+      <span className="pl-1">
+        <span className="font-bold">{t("error.title")}:</span> {{children}}
+      </span>
+    </div>
+
+  )
+}
+
+Error.propTypes = {
+  children: PropTypes.string.isRequired
+}
+
+export {Error}
