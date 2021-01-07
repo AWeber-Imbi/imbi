634570517a80e3d1efb1fe2964fbd9c1b79a69ba
Gavin M. Roy <gavinr@aweber.com>
2021-01-06 19:17:08 -0500

    Fix centering issue

--- ui/src/js/components/ConfirmationDialog/ConfirmationDialog.jsx
+++ ui/src/js/components/ConfirmationDialog/ConfirmationDialog.jsx
@@ -33,6 +33,7 @@ function ConfirmationDialog({title, children, confirmationButtonText, mode, onCa
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
           <div className="absolute inset-0 bg-gray-500 opacity-75" />
         </div>
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
              role="dialog" aria-modal="true" aria-labelledby="modal-headline">
           <div className="sm:flex sm:items-start">
