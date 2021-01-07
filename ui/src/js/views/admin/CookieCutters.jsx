5809bf700ad68ea82268beca4464fbf6e1219dd2
Gavin M. Roy <gavinr@aweber.com>
2021-01-06 21:00:18 -0500

    New pattern for loading metadata/settings

--- ui/src/js/views/admin/CookieCutters.jsx
+++ ui/src/js/views/admin/CookieCutters.jsx
@@ -1,86 +1,92 @@
-import React, {useEffect, useState} from "react"
+import React, {Fragment, useContext, useEffect, useState} from "react"
 import {useTranslation} from 'react-i18next'
 
-
-import {CRUD} from "../../components"
+import {CRUD, Error} from "../../components"
+import {FetchContext} from "../../contexts"
+import {fetchProjectTypes} from "../../settings"
 import {jsonSchema} from "../../schema/CookieCutter"
-import {useFetch} from "../../hooks"
 
 export function CookieCutters() {
-  const [projectTypes, dataErrorMessage] = useFetch("/settings/project_types", [], false, 0)
-  const [projectTypeOptions, setProjectTypeOptions] = useState(projectTypes)
+  const fetch = useContext(FetchContext)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [projectTypes, setProjectTypes] = useState(null)
+  const {t} = useTranslation()
 
   useEffect(() => {
-    const options = []
-    projectTypes.map((projectType) => {
-      options.push({
-        label: projectType.name,
-        value: projectType.name
-      })
-    })
-    setProjectTypeOptions(options)
+    if (projectTypes === null) {
+      fetchProjectTypes(
+        fetch, true,
+        (result) => {
+          setProjectTypes(result)
+        },
+        (error) => {setErrorMessage(error)})
+    }
   }, [projectTypes])
 
-  const {t} = useTranslation()
   return (
-    <CRUD addPath="/admin/cookie_cutter"
-          collectionIcon="fas cookie"
-          collectionName={t("admin.cookieCutters.collectionName")}
-          collectionPath="/settings/cookie_cutters"
-          columns={[
-            {
-              title: t("common.name"),
-              name: "name",
-              type: "text",
-              tableOptions: {
-                className: "min-w-sm"
+    <Fragment>
+      {errorMessage && <Error>{{errorMessage}}</Error>}
+      {!projectTypes && <div>Loading</div>}
+      {projectTypes && (
+      <CRUD addPath="/admin/cookie_cutter"
+            collectionIcon="fas cookie"
+            collectionName={t("admin.cookieCutters.collectionName")}
+            collectionPath="/settings/cookie_cutters"
+            columns={[
+              {
+                title: t("common.name"),
+                name: "name",
+                type: "text",
+                tableOptions: {
+                  className: "min-w-sm"
+                }
+              },
+              {
+                title: t("admin.cookieCutters.type"),
+                name: "type",
+                type: "select",
+                options: [
+                  {"label": "Dashboard", value: "dashboard"},
+                  {"label": "Project", value: "project"},
+                ],
+                tableOptions: {
+                  className: "min-w-sm"
+                }
+              },
+              {
+                title: t("admin.projectTypes.itemName"),
+                name: "project_type",
+                type: "select",
+                options: projectTypes,
+                tableOptions: {
+                  className: "min-w-sm"
+                }
+              },
+              {
+                title: t("common.description"),
+                name: "description",
+                type: "textarea",
+                tableOptions: {
+                  className: "max-w-lg truncate"
+                }
+              },
+              {
+                title: t("admin.cookieCutters.url"),
+                name: "url",
+                description: t("admin.cookieCutters.urlDescription"),
+                type: "text",
+                tableOptions: {
+                  className: "w-min"
+                }
               }
-            },
-            {
-              title: t("admin.cookieCutters.type"),
-              name: "type",
-              type: "select",
-              options: [
-                {"label": "Dashboard", value: "dashboard"},
-                {"label": "Project", value: "project"},
-              ],
-              tableOptions: {
-                className: "min-w-sm"
-              }
-            },
-            {
-              title: t("admin.projectTypes.itemName"),
-              name: "project_type",
-              type: "select",
-              options: projectTypeOptions,
-              tableOptions: {
-                className: "min-w-sm"
-              }
-            },
-            {
-              title: t("common.description"),
-              name: "description",
-              type: "textarea",
-              tableOptions: {
-                className: "max-w-lg truncate"
-              }
-            },
-            {
-              title: t("admin.cookieCutters.url"),
-              name: "url",
-              description: t("admin.cookieCutters.urlDescription"),
-              type: "text",
-              tableOptions: {
-                className: "w-min"
-              }
-            }
-          ]}
-          errorStrings={{
-            "Unique Violation":  t("admin.cookieCutters.errors.uniqueViolation")
-          }}
-          itemKey="name"
-          itemName={t("admin.cookieCutters.itemName")}
-          itemPath="/admin/cookie_cutter/{{value}}"
-          jsonSchema={jsonSchema}/>
+            ]}
+            errorStrings={{
+              "Unique Violation": t("admin.cookieCutters.errors.uniqueViolation")
+            }}
+            itemKey="name"
+            itemName={t("admin.cookieCutters.itemName")}
+            itemPath="/admin/cookie_cutter/{{value}}"
+            jsonSchema={jsonSchema}/>)}
+    </Fragment>
   )
 }
