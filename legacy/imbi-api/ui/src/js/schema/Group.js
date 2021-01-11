be10da6f4bacd78e1e0f4d094a10a39d045fc4ff
Gavin M. Roy <gavinr@aweber.com>
2021-01-11 18:33:56 -0500

    Fix the group schema

--- legacy/imbi-api/ui/src/js/schema/Group.js
+++ legacy/imbi-api/ui/src/js/schema/Group.js
@@ -1,6 +1,38 @@
 import PropTypes from "prop-types"
 
-export const Group = {
+export const jsonSchema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+      minLength: 3
+    },
+    group_types: {
+      type: "string",
+      enum: ["internal", "ldap"]
+    },
+    external_id: {
+      oneOf: [
+        {type: "string"},
+        {type: "null"}
+      ]
+    },
+    permissions: {
+      type: "array",
+      items: {
+        type: "string"
+      }
+    }
+  },
+  additionalProperties: false,
+  required: [
+    "name",
+    "group_types"
+  ]
+}
+
+export const propTypes = {
   name: PropTypes.string.isRequired,
   group_type: PropTypes.oneOf(["internal", "ldap"]).isRequired,
   external_id: PropTypes.string,
