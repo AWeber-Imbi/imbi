371f1d507bfc3bb638092990f059056eff0b4c3d
Gavin M. Roy <gavinr@aweber.com>
2021-01-11 23:31:48 -0500

    Add config file for CI testing

--- ci/test.sh
+++ ci/test.sh
@@ -23,6 +23,42 @@ export SESSION_REDIS_URL=redis://redis:6379/0
 export STATS_REDIS_URL=redis://redis:6379/1
 export POSTGRES_URL=postgres://postgres@postgres:5432/postgres
 EOF
+
+cat > build/test.yaml <<EOF
+---
+ldap:
+  enabled: true
+  host: ldap
+  port: 389
+  ssl: false
+  groups_dn: ou=groups,dc=example,dc=org
+  users_dn: ou=users,dc=example,dc=org
+  username: cn
+  pool_size: 5
+postgres:
+  url: postgres://postgres@postgres:5432/postgres
+session:
+  redis_url: redis://redis:6379/0
+stats:
+  redis_url: redis://redis:6379/1
+logging:
+  version: 1
+  formatters:
+    verbose:
+      format: "%(levelname) -10s %(asctime)s %(name) -20s %(funcName) -25s: %(message)s"
+      datefmt: "%Y-%m-%d %H:%M:%S"
+  handlers:
+    console:
+      class: logging.StreamHandler
+      formatter: verbose
+  loggers: {}
+  root:
+    level: CRITICAL
+    propagate: true
+    handlers: [console]
+  disable_existing_loggers: true
+  incremental: false
+EOF
 ls -al
 pip3 install -e '.[testing]'
 make python-tests
