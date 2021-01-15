a107e1cddb5de324606375202140e9f858fd3e4c
Gavin M. Roy <gavinr@aweber.com>
2021-01-15 17:33:59 -0500

    Stub out new pages

---
+++ legacy/imbi-api/ui/src/js/views/OperationsLog/OperationsLog.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types"
+import React from "react"
+import {useTranslation} from "react-i18next"
+
+import {ContentArea} from "../../components"
+import {User} from "../../schema"
+
+function OperationsLog() {
+  const {t} = useTranslation()
+  return(
+    <ContentArea buttonTitle={t("operationsLog.addEntry")}
+                 pageIcon="fas clipboard"
+                 pageTitle={t("operationsLog.title")} />
+  )
+}
+
+OperationsLog.propTypes = {
+  user: PropTypes.exact(User)
+}
+
+export {OperationsLog}
