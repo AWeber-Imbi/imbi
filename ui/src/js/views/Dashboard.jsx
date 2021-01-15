39944d8228a9cb4f947135e0bdfe7ab6451cca69
Gavin M. Roy <gavinr@aweber.com>
2021-01-15 17:34:12 -0500

    Use the new component

--- ui/src/js/views/Dashboard.jsx
+++ ui/src/js/views/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import {useTranslation} from "react-i18next"
 
+import {ContentArea} from "../components/"
+
 export function Dashboard() {
   const {t} = useTranslation()
   return (
-    <h1 className="text-xl text-bold">{t("common.welcome")}</h1>
+    <ContentArea pageIcon="fas chart-line" pageTitle={t("common.welcome")} />
   )
 }
