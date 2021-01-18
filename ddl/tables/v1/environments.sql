92acf2cddbc347667bcf16dcaf1a7628749c1e1c
Gavin M. Roy <gavinr@aweber.com>
2021-01-18 15:54:03 -0500

    Add text_class to environments for highlighting

--- ddl/tables/v1/environments.sql
+++ ddl/tables/v1/environments.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS environments (
   created_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   modified_at TIMESTAMP WITH TIME ZONE,
   description TEXT,
+  text_class  TEXT,
   icon_class  TEXT DEFAULT 'fas fa-mountain'
 );
 
@@ -12,7 +13,8 @@ COMMENT ON TABLE environments IS 'Operational Environments';
 COMMENT ON COLUMN environments.name IS 'Environment name';
 COMMENT ON COLUMN environments.created_at IS 'When the record was created at';
 COMMENT ON COLUMN environments.modified_at IS 'When the record was last modified';
-COMMENT ON COLUMN environments.icon_class IS 'Font Awesome UI icon class';
+COMMENT ON COLUMN environments.text_class IS 'CSS class for when the value is displayed';
+COMMENT ON COLUMN environments.icon_class IS 'CSS icon class';
 
 GRANT SELECT ON environments TO reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON environments TO admin;
