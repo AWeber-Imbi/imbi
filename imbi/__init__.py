017fbdcca2acbc7618b6172c7662ed18815668fa
Gavin M. Roy <gavinr@aweber.com>
2021-01-21 19:50:41 -0500

    Move versioning to stand-alone VERSION file

--- imbi/__init__.py
+++ imbi/__init__.py
@@ -5,8 +5,8 @@ Imbi Service Management System
 Imbi is Old High German for "Swarm of Bees"
 
 """
+import pkg_resources
 import warnings
 
-version = '0.1.0'
-
+version = pkg_resources.get_distribution('imbi').version
 warnings.simplefilter('ignore', UserWarning)
