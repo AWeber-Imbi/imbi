15a0f8b510ad215892c2dc4f01d93ad4d9d52c23
Gavin M. Roy <gavinr@aweber.com>
2021-01-21 20:34:25 -0500

    Update ci tests to reflect config file and VERSION

--- legacy/imbi-api/ci/test.sh
+++ legacy/imbi-api/ci/test.sh
@@ -6,24 +6,13 @@ cp -R /source/imbi /tmp/test/
 cp -R /source/scaffolding /tmp/test/
 cp -R /source/tests /tmp/test/
 cp /source/setup.* /tmp/test/
+cp LICENSE /tmp/test/
+cp MANIFEST.in /tmp/test/
+cp VERSION /tmp/test/
 cp /source/Makefile /tmp/test/
 cd /tmp/test
 ln -s /usr/local /tmp/test/env
-cat > .env <<EOF
-export ENVIRONMENT=development
-export LDAP_ENABLED=true
-export LDAP_HOST=ldap
-export LDAP_PORT=389
-export LDAP_SSL=False
-export LDAP_GROUPS_DN=ou=groups,dc=example,dc=org
-export LDAP_USERS_DN=ou=users,dc=example,dc=org
-export LDAP_USERNAME=cn
-export LDAP_POOLSIZE=5
-export SESSION_REDIS_URL=redis://redis:6379/0
-export STATS_REDIS_URL=redis://redis:6379/1
-export POSTGRES_URL=postgres://postgres@postgres:5432/postgres
-EOF
-
+mkdir build
 cat > build/test.yaml <<EOF
 ---
 ldap:
