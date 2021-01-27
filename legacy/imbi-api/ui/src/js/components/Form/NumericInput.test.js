1632a4a30baaaafbc0cc7942f196ea193b5dcfda
Gavin M. Roy <gavinr@aweber.com>
2021-01-26 21:47:13 -0500

    Guessing my precommit hooks are not working

--- legacy/imbi-api/ui/src/js/components/Form/NumericInput.test.js
+++ legacy/imbi-api/ui/src/js/components/Form/NumericInput.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createEvent, fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import { NumericInput } from './NumericInput'
