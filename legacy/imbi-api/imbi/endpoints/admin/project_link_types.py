5c788183c0208bcc3cd66839e2d5cac188e7e186
Gavin M. Roy <gavinr@aweber.com>
2021-01-29 17:04:01 -0500

    Addition of surrogate key, other nits
    - Fix the routes
    - Address extra whitespace

--- legacy/imbi-api/imbi/endpoints/admin/project_link_types.py
+++ legacy/imbi-api/imbi/endpoints/admin/project_link_types.py
@@ -6,18 +6,18 @@ from imbi.endpoints.admin import base
 class CRUDRequestHandler(base.CRUDRequestHandler):
 
     NAME = 'admin-project-link-types'
-    ID_KEY = 'link_type'
-    FIELDS = ['link_type', 'icon_class']
+    ID_KEY = 'id'
+    FIELDS = ['id', 'link_type', 'icon_class']
     DEFAULTS = {'icon_class': 'fas fa-link'}
 
     DELETE_SQL = re.sub(r'\s+', ' ', """\
-    DELETE FROM v1.project_link_types WHERE link_type=%(link_type)s;""")
+    DELETE FROM v1.project_link_types WHERE id=%(id)s;""")
 
     GET_SQL = re.sub(r'\s+', ' ', """\
-    SELECT link_type, created_at, created_by, last_modified_at,
-           last_modified_by, icon_class
+    SELECT id, created_at, created_by, last_modified_at, last_modified_by,
+           link_type, icon_class
       FROM v1.project_link_types
-     WHERE link_type=%(link_type)s;""")
+     WHERE id=%(id)s;""")
 
     PATCH_SQL = re.sub(r'\s+', ' ', """\
     UPDATE v1.project_link_types
@@ -25,9 +25,9 @@ class CRUDRequestHandler(base.CRUDRequestHandler):
            last_modified_at=CURRENT_TIMESTAMP,
            last_modified_by=%(username)s,
            icon_class=%(icon_class)s
-     WHERE link_type=%(current_link_type)s;""")
+     WHERE id=%(id)s;""")
 
     POST_SQL = re.sub(r'\s+', ' ', """\
     INSERT INTO v1.project_link_types (link_type, created_by, icon_class)
          VALUES (%(link_type)s, %(username)s, %(icon_class)s)
-      RETURNING link_type""")
+      RETURNING id""")
