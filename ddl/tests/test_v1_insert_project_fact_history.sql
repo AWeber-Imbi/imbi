77467e8168f277a7febc58b903a3513dad964fb1
Eugene Shults <eugenes@aweber.com>
2021-01-29 10:43:56 -0500

    Adds update to project_fact_history test.'

--- ddl/tests/test_v1_insert_project_fact_history.sql
+++ ddl/tests/test_v1_insert_project_fact_history.sql
@@ -1,9 +1,10 @@
 BEGIN;
-SELECT PLAN(1);
+SELECT PLAN(2);
 --fixtures
 INSERT INTO v1.namespaces (name, created_by, slug, icon_class) VALUES ('test_namespace', 'test_user', 'test_slug', 'test_icon_class');
 INSERT INTO v1.project_types (name, created_by, slug) VALUES ('test_project_type', 'test_user', 'test_slug');
 INSERT INTO v1.projects (namespace, name, created_by, slug, project_type) VALUES ('test_namespace', 'test_project', 'test_user', 'test_slug', 'test_project_type');
+INSERT INTO v1.projects (namespace, name, created_by, slug, project_type) VALUES ('test_namespace', 'test_project_2', 'test_user', 'test_slug_2', 'test_project_type');
 INSERT INTO v1.project_fact_types (project_type, fact_type, created_by) VALUES ('test_project_type', 'test_fact_type', 'test_user');
 INSERT INTO v1.project_fact_type_options (project_type, fact_type, value, created_by) VALUES ('test_project_type', 'test_fact_type', 'test_value', 'test_user');
 INSERT INTO v1.project_facts (namespace, name, fact_type, created_by, value) VALUES ('test_namespace', 'test_project', 'test_fact_type', 'test_user', 'test_value');
@@ -11,6 +12,15 @@ INSERT INTO v1.project_facts (namespace, name, fact_type, created_by, value) VAL
 SELECT results_eq(
   $$SELECT "name" FROM v1.project_fact_history WHERE "name"='test_project'$$,
   $$VALUES ('test_project')$$,
+  'Record exists in v1.project_fact_history');
+
+UPDATE v1.project_facts
+   SET "name" = 'test_project_2'
+ WHERE "name" = 'test_project';
+
+ SELECT results_eq(
+  $$SELECT "name" FROM v1.project_fact_history WHERE "name"='test_project_2'$$,
+  $$VALUES ('test_project_2')$$,
   'Record exists in v1.project_fact_history');
 
 ROLLBACK;
