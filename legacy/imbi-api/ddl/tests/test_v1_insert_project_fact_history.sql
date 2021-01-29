dd70c9ce8f12a53ed757ac393bdae7464c997e0d
eshults5 <eshults5@gmail.com>
2021-01-29 09:42:36 -0500

    Adding trigger function insert_project_fact_history() which inserts c… (#3)
    * Adding trigger function insert_project_fact_history() which inserts corresponding data into project_fact_history upon update or insert into project_facts. Created trigger against project_facts. Also added test to validate trigger.
    
    * Blankline
    
    * Update insert_project_fact_history.sql
    
    Co-authored-by: Eugene Shults <eugenes@aweber.com>

---
+++ legacy/imbi-api/ddl/tests/test_v1_insert_project_fact_history.sql
@@ -0,0 +1,16 @@
+BEGIN;
+SELECT PLAN(1);
+--fixtures
+INSERT INTO v1.namespaces (name, created_by, slug, icon_class) VALUES ('test_namespace', 'test_user', 'test_slug', 'test_icon_class');
+INSERT INTO v1.project_types (name, created_by, slug) VALUES ('test_project_type', 'test_user', 'test_slug');
+INSERT INTO v1.projects (namespace, name, created_by, slug, project_type) VALUES ('test_namespace', 'test_project', 'test_user', 'test_slug', 'test_project_type');
+INSERT INTO v1.project_fact_types (project_type, fact_type, created_by) VALUES ('test_project_type', 'test_fact_type', 'test_user');
+INSERT INTO v1.project_fact_type_options (project_type, fact_type, value, created_by) VALUES ('test_project_type', 'test_fact_type', 'test_value', 'test_user');
+INSERT INTO v1.project_facts (namespace, name, fact_type, created_by, value) VALUES ('test_namespace', 'test_project', 'test_fact_type', 'test_user', 'test_value');
+
+SELECT results_eq(
+  $$SELECT "name" FROM v1.project_fact_history WHERE "name"='test_project'$$,
+  $$VALUES ('test_project')$$,
+  'Record exists in v1.project_fact_history');
+
+ROLLBACK;
