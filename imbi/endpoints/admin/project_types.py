cb511da63fdc914ad0e0f6daaf8b97be2819180f
Gavin M. Roy <gavinr@aweber.com>
2021-01-29 16:57:26 -0500

    Reflect addition of surrogate key

--- imbi/endpoints/admin/project_types.py
+++ imbi/endpoints/admin/project_types.py
@@ -6,17 +6,18 @@ from imbi.endpoints.admin import base
 class CRUDRequestHandler(base.CRUDRequestHandler):
 
     NAME = 'admin-project-types'
-    ID_KEY = 'name'
-    FIELDS = ['name', 'description', 'icon_class']
+    ID_KEY = 'id'
+    FIELDS = ['id', 'name', 'description', 'icon_class']
     DEFAULTS = {'icon_class': 'fas fa-folder'}
 
-    DELETE_SQL = 'DELETE FROM v1.project_types WHERE "name"=%(name)s;'
+    DELETE_SQL = 'DELETE FROM v1.project_types WHERE id=%(id)s;'
 
     GET_SQL = re.sub(r'\s+', ' ', """\
-    SELECT "name", created_at, created_by, last_modified_at, last_modified_by,
+    SELECT id, "name", created_at, created_by, 
+           last_modified_at, last_modified_by,
            description, slug, icon_class
       FROM v1.project_types
-     WHERE "name"=%(name)s;""")
+     WHERE id=%(id)s;""")
 
     PATCH_SQL = re.sub(r'\s+', ' ', """\
     UPDATE v1.project_types
@@ -26,11 +27,11 @@ class CRUDRequestHandler(base.CRUDRequestHandler):
            description=%(description)s,
            slug=%(slug)s,
            icon_class=%(icon_class)s
-     WHERE "name"=%(name)s;""")
+     WHERE id=%(id)s;""")
 
     POST_SQL = re.sub(r'\s+', ' ', """\
     INSERT INTO v1.project_types
                 ("name", created_by, description, slug, icon_class)
          VALUES (%(name)s, %(username)s, %(description)s, %(slug)s,
                  %(icon_class)s)
-      RETURNING "name";""")
+      RETURNING id;""")
