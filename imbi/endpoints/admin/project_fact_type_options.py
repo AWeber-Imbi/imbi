10fa9b5847526524cbc11d6185f67be962ed2e3e
Gavin M. Roy <gavinr@aweber.com>
2021-01-29 17:20:34 -0500

    More surrogate key updates
    - Do a little DRY refactoring

--- imbi/endpoints/admin/project_fact_type_options.py
+++ imbi/endpoints/admin/project_fact_type_options.py
@@ -6,38 +6,29 @@ from imbi.endpoints.admin import base
 class CRUDRequestHandler(base.CRUDRequestHandler):
 
     NAME = 'admin-project-fact-type-options'
-    ID_KEY = ['project_type', 'fact_type', 'value']
-    FIELDS = ['project_type', 'fact_type', 'value', 'score']
+    ID_KEY = ['id']
+    FIELDS = ['id', 'fact_type_id', 'value', 'score']
 
     DELETE_SQL = re.sub(r'\s+', ' ', """\
-    DELETE FROM v1.project_fact_type_options
-     WHERE project_type=%(project_type)s
-       AND fact_type=%(fact_type)s
-       AND value=%(value)s""")
+    DELETE FROM v1.project_fact_type_options WHERE id=%(id)s""")
 
     GET_SQL = re.sub(r'\s+', ' ', """\
-    SELECT project_type, fact_type, value, created_at, created_by,
-           last_modified_at, last_modified_by, score
+    SELECT id, fact_type_id, created_at, created_by,
+           last_modified_at, last_modified_by, value, score
       FROM v1.project_fact_type_options
-     WHERE project_type=%(project_type)s
-       AND fact_type=%(fact_type)s
-       AND value=%(value)s""")
+     WHERE id=%(id)s""")
 
     PATCH_SQL = re.sub(r'\s+', ' ', """\
     UPDATE v1.project_fact_type_options
-       SET project_type=%(project_type)s,
-           fact_type=%(fact_type)s,
-           value=%(value)s,
+       SET fact_type_id=%(fact_type_id)s,
            last_modified_at=CURRENT_TIMESTAMP,
            last_modified_by=%(username)s,
+           value=%(value)s,
            score=%(score)s
-     WHERE project_type=%(current_project_type)s
-       AND fact_type=%(current_fact_type)s
-       AND value=%(current_value)s""")
+     WHERE id=%(id)s""")
 
     POST_SQL = re.sub(r'\s+', ' ', """\
     INSERT INTO v1.project_fact_type_options
-                (project_type, fact_type, value, created_by, score)
-         VALUES (%(project_type)s, %(fact_type)s, %(value)s, %(username)s,
-                 %(score)s)
-      RETURNING project_type, fact_type, value;""")
+                (fact_type_id, value, created_by, score)
+         VALUES (%(fact_type_id)s, %(value)s, %(username)s, %(score)s)
+      RETURNING id;""")
