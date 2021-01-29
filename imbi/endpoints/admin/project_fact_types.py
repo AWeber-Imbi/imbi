7f726ac551851f6059b66c80d7aabee3f73b6478
Gavin M. Roy <gavinr@aweber.com>
2021-01-29 17:11:12 -0500

    Surrogate key changes for project fact types

--- imbi/endpoints/admin/project_fact_types.py
+++ imbi/endpoints/admin/project_fact_types.py
@@ -6,33 +6,29 @@ from imbi.endpoints.admin import base
 class CRUDRequestHandler(base.CRUDRequestHandler):
 
     NAME = 'admin-project-fact-types'
-    ID_KEY = ['project_type', 'fact_type']
-    FIELDS = ['project_type', 'fact_type', 'weight']
+    ID_KEY = ['id']
+    FIELDS = ['id', 'project_type_id', 'fact_type', 'weight']
 
     DELETE_SQL = re.sub(r'\s+', ' ', """\
-    DELETE FROM v1.project_fact_types
-     WHERE project_type=%(project_type)s
-       AND fact_type=%(fact_type)s;""")
+    DELETE FROM v1.project_fact_types WHERE id=%(id)s;""")
 
     GET_SQL = re.sub(r'\s+', ' ', """\
-    SELECT project_type, fact_type, created_at, created_by, last_modified_at,
-           last_modified_by, weight
+    SELECT id, project_type_id, fact_type, created_at, created_by,
+           last_modified_at, last_modified_by, weight
       FROM v1.project_fact_types
-     WHERE project_type=%(project_type)s
-       AND fact_type=%(fact_type)s;""")
+     WHERE id=%(id)s;""")
 
     PATCH_SQL = re.sub(r'\s+', ' ', """\
     UPDATE v1.project_fact_types
-       SET project_type=%(project_type)s,
+       SET project_type_id=%(project_type_id)s,
            fact_type=%(fact_type)s,
            last_modified_at=CURRENT_TIMESTAMP,
            last_modified_by=%(username)s,
            weight=%(weight)s
-     WHERE project_type=%(current_project_type)s
-       AND fact_type=%(current_fact_type)s;""")
+     WHERE id=%(id)s;""")
 
     POST_SQL = re.sub(r'\s+', ' ', """\
     INSERT INTO v1.project_fact_types
-                (project_type, fact_type, created_by, weight)
-         VALUES (%(project_type)s, %(fact_type)s, %(username)s, %(weight)s)
-      RETURNING project_type, fact_type;""")
+                (project_type_id, fact_type, created_by, weight)
+         VALUES (%(project_type_id)s, %(fact_type)s, %(username)s, %(weight)s)
+      RETURNING id;""")
