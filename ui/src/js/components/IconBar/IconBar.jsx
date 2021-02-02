86a72bd3ad790e8985ac68e67955e3b04bfd3603
Gavin M. Roy <gavinr@aweber.com>
2021-02-02 14:46:48 -0500

    New component types
    - Icon bar displays a horizontal group of icons with tooltips that display a title of the icon
    - Badge is a simple badge with predefined color schemes

---
+++ ui/src/js/components/IconBar/IconBar.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import { Icon, Tooltip } from '..'
+
+function IconBar({ icons }) {
+  return (
+    <div className="text-gray-500 ">
+      {icons.map((icon) => {
+        if (icon.url !== undefined && icon.url !== null)
+          return (
+            <Tooltip
+              className="ml-1.5"
+              key={`icon-${icon.title}`}
+              value={icon.title}>
+              <a
+                href={icon.url}
+                className="mr-3 hover:text-blue-700"
+                key={`icon-${icon.title}`}
+                target="_new">
+                <Icon icon={icon.icon} />
+              </a>
+            </Tooltip>
+          )
+        return (
+          <Tooltip
+            className="ml-1.5"
+            key={`icon-${icon.title}`}
+            value={icon.title}>
+            <Icon className="mr-3" icon={icon.icon} />
+          </Tooltip>
+        )
+      })}
+    </div>
+  )
+}
+
+IconBar.propTypes = {
+  icons: PropTypes.arrayOf(
+    PropTypes.exact({
+      icon: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      url: PropTypes.string
+    })
+  )
+}
+
+export { IconBar }
