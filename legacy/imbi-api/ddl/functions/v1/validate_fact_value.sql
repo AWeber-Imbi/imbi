ddc9857241e33ed058e8a632e509082a5266f357
Gavin M. Roy <gavinr@aweber.com>
2021-03-03 19:53:03 -0500

    DDL updates for v1.project_facts, simplify audit
    We dont care about about who created vs updated, just keep who last updated the fact and when they did so

--- legacy/imbi-api/ddl/functions/v1/validate_fact_value.sql
+++ legacy/imbi-api/ddl/functions/v1/validate_fact_value.sql
@@ -1,6 +1,6 @@
 SET search_path = v1;
 
-CREATE FUNCTION validate_fact_value() RETURNS trigger
+CREATE OR REPLACE FUNCTION validate_fact_value() RETURNS trigger
        LANGUAGE plpgsql
        SECURITY DEFINER AS $$
   DECLARE
@@ -18,7 +18,8 @@ CREATE FUNCTION validate_fact_value() RETURNS trigger
        WHERE fact_type_id = NEW.fact_type_id
          AND value = NEW.value;
       IF NOT FOUND THEN
-        RAISE EXCEPTION 'value not found in v1.project_fact_type_enums';
+        RAISE EXCEPTION 'Value "%" for % (%) not found in v1.project_fact_type_enums',
+           NEW.value, fact_type.name, NEW.fact_type_id;
       END IF;
     ELSIF (fact_type.fact_type = 'range') THEN
       SELECT * INTO fact_type_range
@@ -26,7 +27,8 @@ CREATE FUNCTION validate_fact_value() RETURNS trigger
        WHERE fact_type_id = NEW.fact_type_id
          AND NEW.value::NUMERIC(9,2) BETWEEN min_value AND max_value;
       IF NOT FOUND THEN
-        RAISE EXCEPTION 'value not found in v1.project_fact_type_ranges';
+        RAISE EXCEPTION '"%" for % (%) not found in v1.project_fact_type_ranges',
+          NEW.value, fact_type.name, NEW.fact_type_id;
       END IF;
     END IF;
     RETURN NEW;
--- legacy/imbi-api/ddl/tables/v1/project_facts.sql
+++ legacy/imbi-api/ddl/tables/v1/project_facts.sql
@@ -3,10 +3,8 @@ SET search_path=v1;
 CREATE TABLE IF NOT EXISTS project_facts (
   project_id        INTEGER                   NOT NULL,
   fact_type_id      INTEGER                   NOT NULL,
-  created_at        TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-  created_by        TEXT                      NOT NULL,
-  last_modified_at  TIMESTAMP WITH TIME ZONE,
-  last_modified_by  TEXT,
+  recorded_at       TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  recorded_by       TEXT                      NOT NULL,
   value             TEXT,
   PRIMARY KEY (project_id, fact_type_id),
   FOREIGN KEY (project_id) REFERENCES projects (id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -16,10 +14,8 @@ CREATE TABLE IF NOT EXISTS project_facts (
 COMMENT ON TABLE project_facts IS 'Stores the current facts for a project';
 COMMENT ON COLUMN project_facts.project_id IS 'The project ID';
 COMMENT ON COLUMN project_facts.fact_type_id IS 'The fact type ID';
-COMMENT ON COLUMN project_facts.created_at IS 'When the record was created at';
-COMMENT ON COLUMN project_facts.created_by IS 'The user created the record';
-COMMENT ON COLUMN project_facts.last_modified_at IS 'When the record was last modified';
-COMMENT ON COLUMN project_facts.last_modified_by IS 'The user that last modified the record';
+COMMENT ON COLUMN project_facts.recorded_at IS 'When the value was last set';
+COMMENT ON COLUMN project_facts.recorded_by IS 'The user that last set the value';
 COMMENT ON COLUMN project_facts.value IS 'The fact value';
 
 GRANT SELECT ON project_facts TO reader;
