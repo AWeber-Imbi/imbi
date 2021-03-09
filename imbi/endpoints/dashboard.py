8f25463ac72de0e4034a150955dd8a2218da1960
Gavin M. Roy <gavinr@aweber.com>
2021-03-08 23:39:27 -0500

    Include additional info from API
    Dashboard: Include the project_type_id for filtering
    Project Facts: Include ui_options
    Projects: Include facts and project score in full view

--- imbi/endpoints/dashboard.py
+++ imbi/endpoints/dashboard.py
@@ -8,7 +8,8 @@ class RequestHandler(base.AuthenticatedRequestHandler):
     NAME = 'dashboard'
 
     PROJECT_COUNTS = re.sub(r'\s+', ' ', """\
-        SELECT b.name AS name,
+        SELECT a.project_type_id,
+               b.name AS name,
                b.plural_name AS plural,
                b.icon_class AS icon,
                b.slug AS slug,
@@ -16,7 +17,7 @@ class RequestHandler(base.AuthenticatedRequestHandler):
           FROM v1.projects AS a
           JOIN v1.project_types AS b
             ON b.id = a.project_type_id
-         GROUP BY b.name, b.plural_name, b.icon_class, b.slug
+         GROUP BY a.project_type_id, b.name, b.plural_name, b.icon_class, b.slug
          ORDER BY count(a.*) DESC""")
 
     async def get(self):
