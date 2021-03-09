8c733e99a533a9dc6100d81b372c9661cc25abae
Gavin M. Roy <gavinr@aweber.com>
2021-03-08 23:17:41 -0500

    Remove debug logging

--- legacy/imbi-api/ui/src/js/components/Icon/Icon.jsx
+++ legacy/imbi-api/ui/src/js/components/Icon/Icon.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 
 function Icon({ icon, ...props }) {
   if (icon === undefined || icon === null) {
-    console.log('Using default icon')
     icon = 'fab empire'
   }
   return <FontAwesomeIcon icon={icon.split(' ')} {...props} />
