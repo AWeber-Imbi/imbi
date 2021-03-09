c413fad98e5a2a31dde04d714e325d593c342b46
Gavin M. Roy <gavinr@aweber.com>
2021-03-08 23:16:29 -0500

    Create Tab related components

---
+++ ui/src/js/components/Tabs/index.js
@@ -0,0 +1,9 @@
+import { Container } from './Container'
+import { Tab } from './Tab'
+import { TabBar } from './TabBar'
+
+export const Tabs = {
+  Container: Container,
+  Tab: Tab,
+  TabBar: TabBar
+}
