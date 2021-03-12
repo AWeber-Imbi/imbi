a506647fcd7649c9631e24e33ce4527f01669411
Gavin M. Roy <gavinr@aweber.com>
2021-03-11 23:38:04 -0500

    Form updates
    - Set value instead of defaultValue in TextInput for state managed above the component
    - Add a new SimpleForm wrapper, we'll eventually use this in ModalForm and MultiSectionForm
    - Add new common form validation functions

--- ui/src/js/components/Form/index.js
+++ ui/src/js/components/Form/index.js
@@ -1,6 +1,7 @@
 import { Field } from './Field'
 import { ModalForm } from './ModalForm'
 import { MultiSectionForm } from './MultiSectionForm'
+import { SimpleForm } from './SimpleForm'
 import { IconSelect } from './IconSelect'
 import { NumericInput } from './NumericInput'
 import { Section } from './Section'
@@ -8,16 +9,20 @@ import { Select } from './Select'
 import { TextArea } from './TextArea'
 import { TextInput } from './TextInput'
 import { Toggle } from './Toggle'
+import { validateObject, validateURLs } from './validate'
 
 export const Form = {
-  Field: Field,
-  IconSelect: IconSelect,
+  SimpleForm: SimpleForm,
   ModalForm: ModalForm,
   MultiSectionForm: MultiSectionForm,
+  Field: Field,
+  IconSelect: IconSelect,
   NumericInput: NumericInput,
   Section: Section,
   Select: Select,
   TextArea: TextArea,
   TextInput: TextInput,
-  Toggle: Toggle
+  Toggle: Toggle,
+  validateObject: validateObject,
+  validateURLs: validateURLs
 }
