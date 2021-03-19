3dec1c211c2c8d27bf4d67f6e50d8deddf23eb97
Gavin M. Roy <gavinr@aweber.com>
2021-03-19 13:56:46 -0400

    Change responsive behavior on dashboard

--- legacy/imbi-api/ui/src/js/components/Stats/Container.jsx
+++ legacy/imbi-api/ui/src/js/components/Stats/Container.jsx
@@ -7,7 +7,7 @@ function Container({ children, title }) {
       {title !== undefined && (
         <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
       )}
-      <div className="mt-5 grid grid-flow-row sm:grid-cols-4 2xl:grid-cols-5 gap-4">
+      <div className="mt-5 grid grid-flow-row sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         {children}
       </div>
     </Fragment>
