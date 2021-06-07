7474bddd9e14be2403d750d85a92b9fc9e739d74
Dave Shawley <daves@aweber.com>
2021-06-07 16:37:43 -0400

    Add missing files to ci/test copy.
    I added some dependencies in 145c217b that need to be present in the CI
    build as well.  I also switched to using tar instead of cp since it will
    do the job more quickly.

--- ci/test.sh
+++ ci/test.sh
@@ -1,16 +1,21 @@
 #!/bin/sh -e
 echo "Setting up tests"
 apk --update add curl-dev gcc git libffi-dev libpq libressl-dev make musl-dev postgresql-dev linux-headers tzdata
-cp -R /source/ddl /tmp/test/
-cp -R /source/imbi /tmp/test/
-cp -R /source/scaffolding /tmp/test/
-cp -R /source/tests /tmp/test/
-cp /source/setup.* /tmp/test/
-cp /source/LICENSE /tmp/test/
-cp /source/MANIFEST.in /tmp/test/
-cp /source/VERSION /tmp/test/
-cp /source/Makefile /tmp/test/
 cd /tmp/test
+tar c -C /source -f - \
+    LICENSE \
+    MANIFEST.in \
+    Makefile \
+    VERSION \
+    bootstrap \
+    docker-compose.yml \
+    ddl \
+    imbi \
+    scaffolding \
+    setup.cfg \
+    setup.py \
+    tests \
+  | tar xf -
 ln -s /usr/local /tmp/test/env
 
 cat > .env <<EOF
