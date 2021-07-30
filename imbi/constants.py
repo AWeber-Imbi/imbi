411a139697a0f717b1cea56346e66bb402f9caf7
Gavin M. Roy <gavinr@aweber.com>
2021-07-30 10:11:26 -0400

    Remove code

--- imbi/constants.py
+++
@@ -1,5 +0,0 @@
-"""
-Common methods and constants
-
-"""
-UUID_PATTERN = r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
--- tests/endpoints/test_metrics.py
+++
@@ -1,9 +0,0 @@
-from tests import base
-
-
-class AsyncHTTPTestCase(base.TestCase):
-
-    def test_status_ok(self):
-        response = self.fetch('/metrics', headers=self.headers)
-        self.assertEqual(response.code, 200)
-        self.validate_response(response)
