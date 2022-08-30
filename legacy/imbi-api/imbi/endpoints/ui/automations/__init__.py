92ba3f717303df00f02ea0f233ea2ae83b473bb5
Dave Shawley <daves@aweber.com>
2022-08-30 11:01:15 -0400

    Implement /ui/automations/sentry/create

--- legacy/imbi-api/imbi/endpoints/ui/automations/__init__.py
+++ legacy/imbi-api/imbi/endpoints/ui/automations/__init__.py
@@ -4,12 +4,14 @@ System Reports
 """
 from tornado import web
 
-from . import gitlab, sonarqube
+from . import gitlab, sentry, sonarqube
 
 URLS = [
     web.url(r'^/ui/automations/gitlab/commit',
             gitlab.InitialCommitRequestHandler),
     web.url(r'^/ui/automations/gitlab/create', gitlab.CreationRequestHandler),
+    web.url(r'^/ui/automations/sentry/create',
+            sentry.ProjectCreationRequestHandler),
     web.url(r'^/ui/automations/sonarqube/create',
-            sonarqube.CreationRequestHandler)
+            sonarqube.CreationRequestHandler),
 ]
