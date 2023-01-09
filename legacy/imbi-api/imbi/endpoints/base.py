fd0f3e7af8339b4303188e2c39a6bbe2a294c696
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2023-01-09 17:03:36 -0500

    Auto-lint base.py

--- legacy/imbi-api/imbi/endpoints/base.py
+++ legacy/imbi-api/imbi/endpoints/base.py
@@ -42,6 +42,7 @@ def require_permission(permission):
     :raises: problemdetails.Problem
 
     """
+
     def _require_permission(f):
         def wrapped(self, *args, **kwargs):
             """Inner-wrapping of the decorator that performs the logic"""
@@ -215,6 +216,7 @@ class RequestHandler(cors.CORSMixin, postgres.RequestHandlerMixin,
 
 class AuthenticatedRequestHandler(RequestHandler):
     """RequestHandler base class for authenticated requests"""
+
     async def prepare(self) -> None:
         await super().prepare()
 
@@ -235,6 +237,7 @@ class AuthenticatedRequestHandler(RequestHandler):
 
 class ValidatingRequestHandler(AuthenticatedRequestHandler):
     """Validates the request against the OpenAPI spec"""
+
     async def prepare(self) -> None:
         await super().prepare()
 
@@ -377,8 +380,8 @@ class CRUDRequestHandler(ValidatingRequestHandler):
 
         original = dict(result.row)
         for key in {
-                'created_at', 'created_by', 'last_modified_at',
-                'last_modified_by'
+            'created_at', 'created_by', 'last_modified_at',
+            'last_modified_by'
         }:
             if key in original:
                 del original[key]
@@ -393,8 +396,8 @@ class CRUDRequestHandler(ValidatingRequestHandler):
 
         # Bail early if there are no changes
         if not {
-                k: original[k]
-                for k in original if k in updated and original[k] != updated[k]
+            k: original[k]
+            for k in original if k in updated and original[k] != updated[k]
         }:
             self._add_self_link(self.request.path)
             self._add_link_header()
@@ -445,7 +448,6 @@ class CRUDRequestHandler(ValidatingRequestHandler):
 
 
 class CollectionRequestHandler(CRUDRequestHandler):
-
     DEFAULTS = {}
     ID_KEY: typing.Union[str, list] = 'id'
     IS_COLLECTION: True
@@ -516,9 +518,9 @@ class PaginationToken:
 
 
 QueryMethod = typing.Callable[[dict[str, typing.Any]],
-                              typing.Coroutine[None, None,
-                                               list[tuple[datetime.datetime,
-                                                          typing.Any]]]]
+typing.Coroutine[None, None,
+list[tuple[datetime.datetime,
+typing.Any]]]]
 """Type signature of the function called by PaginatedRequestHandler
 
 This is something that looks like:
