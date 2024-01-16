d64c414532e266208e7bf733a99cffa57934d475
Dave Shawley <daves@aweber.com>
2024-01-16 17:02:52 -0500

    Add /integrations/{i}/notifications/{n}/filters

--- legacy/imbi-api/imbi/endpoints/integrations/notifications/__init__.py
+++ legacy/imbi-api/imbi/endpoints/integrations/notifications/__init__.py
@@ -1,14 +1,23 @@
 from tornado import web
 
+from . import filters
 from . import notifications
 
 PREFIX = r'^/integrations/(?P<integration_name>[^/]+)/notifications'
+SLUG = r'[\w_\-%\+]+'
 
 URLS = [
     web.url(fr'{PREFIX}/?$',
             notifications.CollectionRequestHandler,
-            name='notifications'),
-    web.url(fr'{PREFIX}/(?P<name>[\w_\-%\+]+)$',
+            name=notifications.CollectionRequestHandler.NAME),
+    web.url(fr'{PREFIX}/(?P<name>{SLUG})$',
             notifications.RecordRequestHandler,
-            name='notification'),
+            name=notifications.RecordRequestHandler.NAME),
+    web.url(fr'{PREFIX}/(?P<notification_name>{SLUG})/filters$',
+            filters.CollectionRequestHandler,
+            name=filters.CollectionRequestHandler.NAME),
+    web.url(
+        fr'{PREFIX}/(?P<notification_name>{SLUG})/filters/(?P<name>{SLUG})$',
+        filters.RecordRequestHandler,
+        name=filters.RecordRequestHandler.NAME),
 ]
