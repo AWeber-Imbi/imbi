453241db492f78c2ba5e6e3c133aefa9efd92047
Dave Shawley <daves@aweber.com>
2024-06-18 16:12:32 -0400

    Use base.AuthenticatedRequestHandler in reports
    This should be slightly more efficient and is certainly more consistent
    with the rest of the codebase.

--- legacy/imbi-api/imbi/endpoints/reports/component_usage.py
+++ legacy/imbi-api/imbi/endpoints/reports/component_usage.py
@@ -1,11 +1,9 @@
 import re
 
-from tornado import web
-
 from imbi.endpoints import base
 
 
-class RequestHandler(base.RequestHandler):
+class RequestHandler(base.AuthenticatedRequestHandler):
     NAME = 'reports-component-usage'
     SQL = re.sub(
         r'\s+', ' ', """\
@@ -18,7 +16,6 @@ class RequestHandler(base.RequestHandler):
          GROUP BY c.name, c.package_url, c.active_version
         """)
 
-    @web.authenticated
     async def get(self) -> None:
         result = await self.postgres_execute(self.SQL, metric_name=self.NAME)
         self.send_response(result.rows)
