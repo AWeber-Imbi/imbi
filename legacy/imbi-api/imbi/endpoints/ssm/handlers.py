cc731a6feaba4aa07378ed036440a41c3a404fdf
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2024-07-02 15:34:33 -0400

    Revert "Remove unnecessary HTTPClientMixin base class"
    This reverts commit 9cb05d5a5a87d598e0e9afff3cc5952eac9f6b5d.
    This is used when refreshing oauth2 tokens.

--- legacy/imbi-api/imbi/endpoints/ssm/handlers.py
+++ legacy/imbi-api/imbi/endpoints/ssm/handlers.py
@@ -7,6 +7,7 @@ import aioboto3
 import aioredis
 import botocore.exceptions
 import jsonpatch
+import sprockets.mixins.http
 from tornado import web
 
 from imbi import errors, oauth2, user
@@ -22,7 +23,8 @@ def path_prefix(template: str, project_slug: str, project_type_slug: str,
         project_type_slug).replace('{namespace_slug}', namespace_slug)
 
 
-class SSMRequestHandler(base.ValidatingRequestHandler):
+class SSMRequestHandler(sprockets.mixins.http.HTTPClientMixin,
+                        base.ValidatingRequestHandler):
     GET_PROJECT_INFO_SQL = re.sub(
         r'\s+', ' ', """\
         SELECT n.id AS namespace_id,
