de32d035d7e88c10433ccca2ca3154999bb5992e
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2024-07-02 13:53:32 -0400

    Use JsonPointer validation in SSM patch models

--- legacy/imbi-api/imbi/endpoints/ssm/models.py
+++ legacy/imbi-api/imbi/endpoints/ssm/models.py
@@ -2,6 +2,8 @@ import typing
 
 import pydantic
 
+from imbi import common
+
 
 class Param(pydantic.BaseModel):
     type: typing.Literal['String', 'SecureString', 'StringList']
@@ -12,21 +14,23 @@ class EnvironmentFieldMixin:
     @pydantic.computed_field
     @property
     def environment(self) -> str:
-        return self.path.split('/')[1]
+        return self.path.get_parts()[0]
 
 
 class RemovePatch(pydantic.BaseModel, EnvironmentFieldMixin):
     op: typing.Literal['remove']
-    path: str
+    path: common.JsonPointer
 
     @pydantic.field_validator('path', mode='after')
-    def validate_path(cls, value: str, info: pydantic.ValidationInfo):
-        if value.count('/') > 1:
+    def validate_path(cls, value: common.JsonPointer,
+                      info: pydantic.ValidationInfo):
+        path_parts = value.get_parts()
+        if len(path_parts) > 1:
             raise ValueError(
                 f'Invalid path: {value}. Can only remove an environment')
 
         environments = info.context.get('environments', [])
-        environment = value.split('/')[1]
+        environment = path_parts[0]
         if environment not in environments:
             raise ValueError(
                 f'Invalid environment "{environment}", must be one of: '
@@ -37,20 +41,20 @@ class RemovePatch(pydantic.BaseModel, EnvironmentFieldMixin):
 
 class ReplacePatch(pydantic.BaseModel, EnvironmentFieldMixin):
     op: typing.Literal['replace']
-    path: str
+    path: common.JsonPointer
     value: str
 
     @pydantic.computed_field
     @property
     def type(self) -> str:
-        return self.path.split('/')[2]
+        return self.path.get_parts()[1]
 
     @pydantic.model_validator(mode='after')
     def validate_replace(self, info: pydantic.ValidationInfo):
         environments = info.context.get('environments', [])
 
         try:
-            _, environment, update_type = self.path.split('/')
+            [environment, update_type] = self.path.get_parts()
         except ValueError:
             raise ValueError(
                 f'Invalid path: {self.value}. Must replace type or value at '
@@ -78,18 +82,20 @@ class ReplacePatch(pydantic.BaseModel, EnvironmentFieldMixin):
 
 class AddPatch(pydantic.BaseModel, EnvironmentFieldMixin):
     op: typing.Literal['add']
-    path: str
+    path: common.JsonPointer
     value: Param
 
     @pydantic.field_validator('path')
     @classmethod
-    def validate_path(cls, value: str, info: pydantic.ValidationInfo):
-        if value.count('/') > 1:
+    def validate_path(cls, value: common.JsonPointer,
+                      info: pydantic.ValidationInfo):
+        path_parts = value.get_parts()
+        if len(path_parts) > 1:
             raise ValueError(
                 f'Invalid path: {value}. Can only add at an environment')
 
         environments = info.context.get('environments', [])
-        environment = value.split('/')[1]
+        environment = path_parts[0]
         if environment not in environments:
             raise ValueError(
                 f'Invalid environment "{environment}", must be one of: '
